fb7bf6c04a34075140ec333bc01cab7772edb633
MariaKonstantinov <konstantinovamariia1@gmail.com>
2022-05-07 22:41:32 +0300

    add functionality to webpage sprint 5

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,32 @@
-/* General functions */
+/* Six Cards - Links */
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
+
+/* Utility functions and variables*/
 function openPopup(popupForm) {
   popupForm.classList.add("popup_opened");
 }
@@ -6,21 +34,25 @@ function openPopup(popupForm) {
 function closePopup(popupForm) {
   popupForm.classList.remove("popup_opened");
 }
+const cardsListElement = document.querySelector(".cards__grid");
+const cardTemplateElement = document.querySelector("#card-template");
 
+/* Enable close button to close all popups */
 const popups = document.querySelectorAll(".popup");
 popups.forEach((popup) => {
   closeButton = popup.querySelector(".button_type_close");
   closeButton.addEventListener("click", () => closePopup(popup));
 });
 
-/* Popup Form - Edit Profile */
+/* Popup - Edit Profile -------------------------------------------- */
 const editButton = document.querySelector(".button_type_edit");
-const popupProfile = document.querySelector(".popup.popup_type_profile");
+const popupProfile = document.querySelector(".popup.popup_type_edit-profile");
 const profileName = document.querySelector(".profile__name");
 const nameInput = document.querySelector(".popup__input_type_name");
 const profileJob = document.querySelector(".profile__job");
 const jobInput = document.querySelector(".popup__input_type_job");
 
+/* Handlers definition */
 function handleEditButtonClick() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
@@ -34,5 +66,88 @@ function handleEditFormSubmit(event) {
   closePopup(popupProfile);
 }
 
+/* Attach handlers */
 editButton.addEventListener("click", handleEditButtonClick);
 popupProfile.addEventListener("submit", handleEditFormSubmit);
+
+/* Popup - New Card ---------------------------------------------- */
+const addButton = document.querySelector(".button_type_add");
+const popupNewPlace = document.querySelector(".popup.popup_type_new-card");
+const imageTitleInput = document.querySelector(
+  ".popup__input_type_image-title"
+);
+const imageLinkInput = document.querySelector(".popup__input_type_image-link");
+
+/* Handlers definition */
+function handleAddButtonClick() {
+  imageTitleInput.value = "";
+  imageLinkInput.value = "";
+  openPopup(popupNewPlace);
+}
+
+function handleAddFormSubmit(event) {
+  event.preventDefault();
+  const newCard = { name: imageTitleInput.value, link: imageLinkInput.value };
+  cardsListElement.prepend(createCard(newCard));
+  closePopup(popupNewPlace);
+}
+
+/* Attach handlers */
+addButton.addEventListener("click", handleAddButtonClick);
+popupNewPlace.addEventListener("submit", handleAddFormSubmit);
+
+/* Popup - Zoom Image -------------------------------------------- */
+const popUpImageZoom = document.querySelector(".popup_type_zoom-card");
+const imageZoom = document.querySelector(".popup__zoom-image");
+const imageZoomTitle = document.querySelector(".popup__zoom-title");
+
+function handleCardImageClick(event) {
+  imageZoom.src = event.target.src;
+  imageZoom.alt = event.target.alt;
+  imageZoomTitle.textContent = event.target.alt;
+  openPopup(popUpImageZoom);
+}
+
+/* Create and Initialize a New Card Object ------------------------ */
+function createCard(card) {
+ 
+  /*Function to Delete a Card*/
+  function handleTrashButtonClick(event) {
+    event.target.closest(".card").remove();
+  }
+
+  /*Function to Change Heart Icon to Black Color*/
+  function handleLikeButtonClick(event) {
+    event.target.classList.toggle("button_style_like-active");
+  }
+
+  const cardElement = cardTemplateElement.content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  const cardTitleElement = cardElement.querySelector(".card__title");
+  cardTitleElement.textContent = card.name;
+
+  const cardImageElement = cardElement.querySelector(".card__image");
+  cardImageElement.src = card.link;
+  cardImageElement.alt = card.name;
+
+  /* Attach handlers */
+  const cardLikeButtonElement = cardElement.querySelector(".button_style_like");
+  cardLikeButtonElement.addEventListener("click", handleLikeButtonClick);
+
+  const cardTrashButtonElement =
+    cardElement.querySelector(".button_type_trash");
+  cardTrashButtonElement.addEventListener("click", handleTrashButtonClick);
+
+  cardImageElement.addEventListener("click", handleCardImageClick);
+
+  return cardElement;
+}
+
+/*Function to Display All Cards*/
+function renderInitialCards() {
+    initialCards.forEach((card) => cardsListElement.prepend(createCard(card)));
+}
+
+renderInitialCards();
