d3d0bda0da3cb50fa86bfef31d38b2a1f97a7c00
MariaKonstantinov <konstantinovamariia1@gmail.com>
2022-11-29 17:11:50 +0200

    card.js

---
+++ scripts/Card.js
@@ -0,0 +1,52 @@
+export class Card {
+  constructor(cardData, cardTemplateElement) {
+    this._text = cardData.name;
+    this._link = cardData.link;
+
+    this._template = cardTemplateElement;
+  }
+
+  // method to "like" a card
+  _handleLikeButtonClick(event) {
+    event.target.classList.toggle("button_style_like-active");
+  }
+
+  // method to delete a card
+  _handleTrashButtonClick(event) {
+    event.target.closest(".card").remove();
+  }
+
+  // method to preview (zoom) a card
+  _handleCardImageClick(event) {
+    imageZoom.src = event.target.src;
+    imageZoom.alt = event.target.alt;
+    imageZoomTitle.textContent = event.target.alt;
+    openPopup(popUpImageZoom);
+  }
+
+  // EVENT LISTENERS
+  _addEventListeners() {
+    const cardLikeButtonElement =
+      this._cardElement.querySelector(".button_style_like");
+    const cardTrashButtonElement =
+      this._cardElement.querySelector(".button_type_trash");
+    const cardImageElement = this._cardElement.querySelector(".card__image");
+
+    cardImageElement.src = this._link;
+    cardImageElement.alt = this._name;
+
+    // handlers
+    cardLikeButtonElement.addEventListener(
+      "click",
+      this._handleLikeButtonClick
+    );
+    cardTrashButtonElement.addEventListener(
+      "click",
+      this._handleTrashButtonClick
+    );
+    cardImageElement.addEventListener("click", this._handleCardImageClick);
+  }
+
+  // public method to display card
+  render() {}
+}
--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@
 // import { toggleButtonToDisabledState, configObject } from "./validation.js";
 import { FormValidator } from "./FormValidator.js";
 import { openPopup, closePopup } from "./utils.js";
+import { Card } from "./Card";
 
 /** Six Cards - Links */
 const initialCards = [
@@ -54,6 +55,7 @@ function resetPopupForm(popup) {
 }
 
 /** Popup - Edit Profile -------------------------------------------- */
+// EDIT POPUP CONSTS
 const editButton = document.querySelector(".button_type_edit");
 const popupProfile = document.querySelector(".popup.popup_type_edit-profile");
 const profileName = document.querySelector(".profile__name");
@@ -61,7 +63,7 @@ const nameInput = document.querySelector(".popup__input_type_name");
 const profileJob = document.querySelector(".profile__job");
 const jobInput = document.querySelector(".popup__input_type_job");
 
-/** Function to fill the Edit Profile popup inputs */
+/** Function to fill the Edit Profile popup inputs ----------------------------->*/
 function fillEditProfileForm() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
@@ -84,15 +86,20 @@ function handleEditFormSubmit(event) {
 editButton.addEventListener("click", handleEditButtonClick);
 popupProfile.addEventListener("submit", handleEditFormSubmit);
 
-/** Popup - New Card ---------------------------------------------- */
+/** ----------------------- Popup - New Card ---------------------------------------------- */
+// CARD POPUP CONSTS
 const addButton = document.querySelector(".button_type_add");
 const popupNewPlace = document.querySelector(".popup.popup_type_new-card");
 const imageTitleInput = document.querySelector(
   ".popup__input_type_image-title"
 );
 const imageLinkInput = document.querySelector(".popup__input_type_image-link");
-
 const submitButton = popupNewPlace.querySelector(".button_type_submit");
+const popUpImageZoom = document.querySelector(".popup_type_zoom-card");
+const imageZoom = document.querySelector(".popup__zoom-image");
+const imageZoomTitle = document.querySelector(".popup__zoom-title");
+
+// ------------------------------------------------>
 
 function handleAddButtonClick() {
   toggleButtonToDisabledState(submitButton, configObject);
@@ -111,52 +118,50 @@ function handleAddFormSubmit(event) {
 addButton.addEventListener("click", handleAddButtonClick);
 popupNewPlace.addEventListener("submit", handleAddFormSubmit);
 
-/** Popup - Zoom Image -------------------------------------------- */
-const popUpImageZoom = document.querySelector(".popup_type_zoom-card");
-const imageZoom = document.querySelector(".popup__zoom-image");
-const imageZoomTitle = document.querySelector(".popup__zoom-title");
+// ------------------------------------------------>
 
-function handleCardImageClick(event) {
-  imageZoom.src = event.target.src;
-  imageZoom.alt = event.target.alt;
-  imageZoomTitle.textContent = event.target.alt;
-  openPopup(popUpImageZoom);
-}
+/** TODO Popup - Zoom Image -------------------------------------------- */
+// function handleCardImageClick(event) {
+//   imageZoom.src = event.target.src;
+//   imageZoom.alt = event.target.alt;
+//   imageZoomTitle.textContent = event.target.alt;
+//   openPopup(popUpImageZoom);
+// } // done
 
 /** Create and Initialize a New Card Object ------------------------ */
 function createCard(card) {
-  /** Function to Delete a Card */
+  /** TODO Function to Delete a Card */
   function handleTrashButtonClick(event) {
     event.target.closest(".card").remove();
-  }
+  } // done
 
-  /** Function to Change Heart Icon to Black Color */
+  /** TODO Function to Change Heart Icon to Black Color - "like" */
   function handleLikeButtonClick(event) {
     event.target.classList.toggle("button_style_like-active");
-  }
+  } // done
 
-  const cardElement = cardTemplateElement.content
-    .querySelector(".card")
-    .cloneNode(true);
+  const cardElement = cardTemplateElement.content // done
+    .querySelector(".card") // done
+    .cloneNode(true); // done
 
   const cardTitleElement = cardElement.querySelector(".card__title");
   cardTitleElement.textContent = card.name;
 
-  const cardImageElement = cardElement.querySelector(".card__image");
-  cardImageElement.src = card.link;
-  cardImageElement.alt = card.name;
+  const cardImageElement = cardElement.querySelector(".card__image"); // done
+  cardImageElement.src = card.link; // done
+  cardImageElement.alt = card.name; // done
 
-  /** Attach handlers */
-  const cardLikeButtonElement = cardElement.querySelector(".button_style_like");
-  cardLikeButtonElement.addEventListener("click", handleLikeButtonClick);
+  /** Attach handlers with callback functions */
+  const cardLikeButtonElement = cardElement.querySelector(".button_style_like"); // done
+  cardLikeButtonElement.addEventListener("click", handleLikeButtonClick); //done
 
   const cardTrashButtonElement =
-    cardElement.querySelector(".button_type_trash");
-  cardTrashButtonElement.addEventListener("click", handleTrashButtonClick);
+    cardElement.querySelector(".button_type_trash"); // done
+  cardTrashButtonElement.addEventListener("click", handleTrashButtonClick); //done
 
-  cardImageElement.addEventListener("click", handleCardImageClick);
+  cardImageElement.addEventListener("click", handleCardImageClick); // done
 
-  return cardElement;
+  return cardElement; // done
 }
 
 /** Function to Display All Cards */
@@ -166,7 +171,7 @@ function renderInitialCards() {
 
 renderInitialCards();
 
-// creating a new formValidator class instance
+// creating a new formValidator class instance ----------------------------->
 const settings = {
   inputSelector: ".popup__input",
   submitButtonSelector: ".button_type_submit",
@@ -186,3 +191,7 @@ formList.forEach((formElement) => {
   // we have all our methods and properties accessed after dot, but we can only use a public method here which is enableValidation
   formValidator.enableValidation();
 });
+
+// creating a new Card class instance ----------------------------->
+
+const card = new Card(cardData, cardTemplateElement);
